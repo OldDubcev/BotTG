42a8d6d35e6a4ab57d668f0d36c6a05e3e94caac
Никита Стародубцев <tigernik_star@mail.ru>
2021-10-13 17:48:53 +0300

    Add logic for bot

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import telebot
 from telebot import types
 import sqlite3
 
-
-client = telebot.TeleBot('2083903747:AAGTeoLnDe5c-IybyKoVZMRJKli5CCd2AX0');
-bot_url= 'https://git.heroku.com/botstirka.git'
+TOKEN = '2083903747:AAGTeoLnDe5c-IybyKoVZMRJKli5CCd2AX0'
+APP_URL = f'https://botstirka.herokuapp.com/{TOKEN}'
+client = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 conn = sqlite3.connect('K:\SQL Lite\BotDB.db', check_same_thread=False)
 cursor = conn.cursor()
@@ -93,25 +94,21 @@ def change_status(message):
     mach_stat = 1
     db_table_val(user_id=us_id, user_name=us_name, machine_id=mach_id, machine_status=mach_stat)
 
-#ТАЙМЕР
-def task(bot, job):
-    bot.send_message(job.context, text='Стирка окончена, открой машинку!')
 
-def set_timer(bot, update, job_queue, chat_data):
-    
-    # создаём задачу task в очереди job_queue
-    # передаём ей идентификатор текущего чата
-    # идентификатор далее будет доступен через job.context
-    
-    delay = 5 # количество секунд
-    job = job_queue.run_once(task, delay, context=update.message.chat_id)
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    client.process_new_updates([update])
+    return '!', 200
 
-    # Регистрируем созданную задачу в пользовательских данных
-    chat_data['job'] = job
-    
-    # Сообщаем о том, что таймер установлен
-    client.send_message('Таймер установлен на ' + str(delay) + ' секунд' )
 
-        
-client.polling()
-        
+@server.route('/')
+def webhook():
+    client.remove_webhook()
+    client.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
