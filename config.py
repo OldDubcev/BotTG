4c54f3c3540d5076340674dd0c59f46d9a3847e1
OldDubcev <92444612+OldDubcev@users.noreply.github.com>
2021-10-16 01:36:10 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-host = 'ec2-52-210-120-210.eu-west-1.compute.amazonaws.com'
-user = 'rjnmasdeuyhcpn'
-password = 'bd504f1761418197d414f55520a7afb028eaacf0daa21da8caa8b4b7016537a6'
-db_name = 'dfifnk8obqck38'
