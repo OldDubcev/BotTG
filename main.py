0be3235d87dc6dd502111fc05e0ae600d5de4219
OldDubcev <92444612+OldDubcev@users.noreply.github.com>
2021-10-16 01:46:28 +0300

    NEW

--- main.py
+++ main.py
@@ -45,14 +45,10 @@ def mode_machine(message):
     rmk2.add(types.KeyboardButton('Начать стирку'))
     if message.text == '45':
         bot.send_message(message.chat.id, 'Закрой машинку и начни стирку', reply_markup=rmk2)
-        db.update_status(False, message.from_user.id)
-        bot.send_message(message.chat.id,db.Error, reply_markup=rmk2)
-        bot.send_message(message.chat.id,db.version, reply_markup=rmk2)
+        db.update_status(False, 4)
     elif message.text == '60':
         bot.send_message(message.chat.id, 'Закрой машинку и начни стирку', reply_markup=rmk2)
-        db.update_status(False, message.from_user.id)
-        bot.send_message(message.chat.id,db.Error, reply_markup=rmk2)
-        bot.send_message(message.chat.id,db.version, reply_markup=rmk2)
+        db.update_status(False, 4)
 
 
 
